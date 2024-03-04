b2e734b7b39789ad7fe697297040e4f9074b276c
Criftcking <86767725+Criftcking@users.noreply.github.com>
2024-03-04 06:29:13 +0100

    Update spotdown.py

--- spotdown.py
+++ spotdown.py
@@ -5,6 +5,7 @@ import colorama
 from colorama import Fore
 import spotdl
 spotdl
+import time
 colorama.init()
 def check_and_install_library(library_name):
     try:
@@ -22,42 +23,47 @@ def limpiar():
         else:  # Linux/Unix/Mac
             os.system('clear')
             
-limpiar()
-def menu():
-    print(Fore.MAGENTA+"""
-            
-      ██████  ██▓███   ▒█████  ▄▄▄█████▓ ██▓  █████▒▓██   ██▓    ███▄ ▄███▓ █    ██   ██████  ██▓ ▄████▄  
-    ▒██    ▒ ▓██░  ██▒▒██▒  ██▒▓  ██▒ ▓▒▓██▒▓██   ▒  ▒██  ██▒   ▓██▒▀█▀ ██▒ ██  ▓██▒▒██    ▒ ▓██▒▒██▀ ▀█  
-    ░ ▓██▄   ▓██░ ██▓▒▒██░  ██▒▒ ▓██░ ▒░▒██▒▒████ ░   ▒██ ██░   ▓██    ▓██░▓██  ▒██░░ ▓██▄   ▒██▒▒▓█    ▄ 
-      ▒   ██▒▒██▄█▓▒ ▒▒██   ██░░ ▓██▓ ░ ░██░░▓█▒  ░   ░ ▐██▓░   ▒██    ▒██ ▓▓█  ░██░  ▒   ██▒░██░▒▓▓▄ ▄██▒
-    ▒██████▒▒▒██▒ ░  ░░ ████▓▒░  ▒██▒ ░ ░██░░▒█░      ░ ██▒▓░   ▒██▒   ░██▒▒▒█████▓ ▒██████▒▒░██░▒ ▓███▀ ░
-    ▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░░ ▒░▒░▒░   ▒ ░░   ░▓   ▒ ░       ██▒▒▒    ░ ▒░   ░  ░░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░░▓  ░ ░▒ ▒  ░
-    ░ ░▒  ░ ░░▒ ░       ░ ▒ ▒░     ░     ▒ ░ ░       ▓██ ░▒░    ░  ░      ░░░▒░ ░ ░ ░ ░▒  ░ ░ ▒ ░  ░  ▒   
-    ░  ░  ░  ░░       ░ ░ ░ ▒    ░       ▒ ░ ░ ░     ▒ ▒ ░░     ░      ░    ░░░ ░ ░ ░  ░  ░   ▒ ░░        
-        ░               ░ ░            ░           ░ ░               ░      ░           ░   ░  ░ ░      
-                                                    ░ ░                                         ░        
-"""+Fore.RESET)
-    
-def options():
-    print(Fore.MAGENTA+"""
-    |==Downloader Menu==|
-
-    1- Install Requirements
-    2- Spotify Music Download (URL)
-    3- Exit
-    """+Fore.RESET)    
-menu()
-options()
-
-option = int(input(Fore.LIGHTMAGENTA_EX+"[Select option]========>"+Fore.RESET))
-if option == 1:
-    os.system("spotdl --download-ffmpeg")
-elif option == 2:
+def reset():
     limpiar()
+    def menu():
+        print(Fore.MAGENTA+"""
+                
+        ██████  ██▓███   ▒█████  ▄▄▄█████▓ ██▓  █████▒▓██   ██▓    ███▄ ▄███▓ █    ██   ██████  ██▓ ▄████▄  
+        ▒██    ▒ ▓██░  ██▒▒██▒  ██▒▓  ██▒ ▓▒▓██▒▓██   ▒  ▒██  ██▒   ▓██▒▀█▀ ██▒ ██  ▓██▒▒██    ▒ ▓██▒▒██▀ ▀█  
+        ░ ▓██▄   ▓██░ ██▓▒▒██░  ██▒▒ ▓██░ ▒░▒██▒▒████ ░   ▒██ ██░   ▓██    ▓██░▓██  ▒██░░ ▓██▄   ▒██▒▒▓█    ▄ 
+        ▒   ██▒▒██▄█▓▒ ▒▒██   ██░░ ▓██▓ ░ ░██░░▓█▒  ░   ░ ▐██▓░   ▒██    ▒██ ▓▓█  ░██░  ▒   ██▒░██░▒▓▓▄ ▄██▒
+        ▒██████▒▒▒██▒ ░  ░░ ████▓▒░  ▒██▒ ░ ░██░░▒█░      ░ ██▒▓░   ▒██▒   ░██▒▒▒█████▓ ▒██████▒▒░██░▒ ▓███▀ ░
+        ▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░░ ▒░▒░▒░   ▒ ░░   ░▓   ▒ ░       ██▒▒▒    ░ ▒░   ░  ░░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░░▓  ░ ░▒ ▒  ░
+        ░ ░▒  ░ ░░▒ ░       ░ ▒ ▒░     ░     ▒ ░ ░       ▓██ ░▒░    ░  ░      ░░░▒░ ░ ░ ░ ░▒  ░ ░ ▒ ░  ░  ▒   
+        ░  ░  ░  ░░       ░ ░ ░ ▒    ░       ▒ ░ ░ ░     ▒ ▒ ░░     ░      ░    ░░░ ░ ░ ░  ░  ░   ▒ ░░        
+            ░               ░ ░            ░           ░ ░               ░      ░           ░   ░  ░ ░      
+                                                        ░ ░                                         ░        
+    """+Fore.RESET)
+        
+    def options():
+        print(Fore.MAGENTA+"""
+        |==Downloader Menu==|
+
+        1- Install Requirements
+        2- Spotify Music Download (URL)
+        3- Exit
+        """+Fore.RESET)    
     menu()
-    url = str(input(Fore.LIGHTMAGENTA_EX+"[Paste URL]========>"+Fore.RESET))
-    os.system(f'spotdl --output "music" {url}')
-    print(Fore.LIGHTMAGENTA_EX+"MUSIC SAVE IN '/music' folder")
-else:
-    limpiar()
-    pass
+    options()
+
+    option = int(input(Fore.LIGHTMAGENTA_EX+"[Select option]========>"+Fore.RESET))
+    if option == 1:
+        os.system("spotdl --download-ffmpeg")
+        reset()
+    elif option == 2:
+        limpiar()
+        menu()
+        url = str(input(Fore.LIGHTMAGENTA_EX+"[Paste URL]========>"+Fore.RESET))
+        os.system(f'spotdl --output "music" {url}')
+        print(Fore.LIGHTMAGENTA_EX+"MUSIC SAVE IN '/music' folder")
+        time.sleep(5)
+        reset()
+    else:
+        pass
+
+reset()
